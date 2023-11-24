188ed76358c6d7bf8541117b1c2bdc442fc1ebfa
Takahiro Shinagawa <shina@ecc.u-tokyo.ac.jp>
2023-11-24 15:34:51 +0900

    debug5

--- netlify/edge-functions/sigos_redir.js
+++ netlify/edge-functions/sigos_redir.js
@@ -1,11 +1,15 @@
 export default async (request) => {
         var url = new URL(request.url);
+        console.log(url);
         const params = new URLSearchParams(url.search);
         if (params.has('ComSys2023')) {
                 url.pathname = '/event/comsys2023/';
                 url.search = '';
         } else {
-                url = request.url.replace(/^.*\/sig\/os/, "http://www.ipsj.or.jp/sig/os");
+                url.hostname = 'www.ipsj.or.jp';
+                url.protocol = 'http:';
+                url.port = '';
+                url.search = url.search.replace(/=$/, '');
         }
         console.log(url);
         return Response.redirect(url);
