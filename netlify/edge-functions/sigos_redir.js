dd31c6ecd97d79d45211c514f64660959e5b8c8e
Takahiro Shinagawa <shina@ecc.u-tokyo.ac.jp>
2023-11-24 14:58:12 +0900

    debug

--- netlify/edge-functions/sigos_redir.js
+++ netlify/edge-functions/sigos_redir.js
@@ -3,9 +3,11 @@ export default async (request) => {
         const params = new URLSearchParams(url.search);
         if (params.has('ComSys2023')) {
                 const new_url = new URL("/event/comsys2023/", request.url);
+                console.log(new_url);
                 return new_url;
         }
         url.hostname = 'www.ipsj.or.jp';
         url.port = 80;
+        console.log(url);
         return Response.redirect(url);
 };
